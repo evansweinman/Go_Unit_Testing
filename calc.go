6199294bb28cec81d59a25bb2c8d72a480376cba
evansweinman <94131053+evansweinman@users.noreply.github.com>
2021-11-18 16:03:39 -0500

    Add files via upload
    Go files for unit testing

---
+++ calc.go
@@ -0,0 +1,10 @@
+package calc
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func Sub(x, y int) int {
+	//return y - x
+	return x - y
+}
---
+++ calc_test.go
@@ -0,0 +1,24 @@
+package calc_test
+
+import (
+	"calctest/calc"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	got := calc.Add(2, 1)
+	expected := 3
+
+	if got != expected {
+		t.Errorf("not expected result, got: %v expected: %v", got, expected)
+	}
+}
+
+func TestSub(t *testing.T) {
+	got := calc.Sub(2, 1)
+	expected := 1
+
+	if got != expected {
+		t.Errorf("not expected result, got: %v expected: %v", got, expected)
+	}
+}
